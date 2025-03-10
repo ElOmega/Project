622d952d4acd02731bc5e3149712e6df51639407
ElOmega <53097833+ElOmega@users.noreply.github.com>
2025-03-10 15:31:27 +0100

    Add files via upload

---
+++ Projet_ELP/ELM/Types.elm
@@ -0,0 +1,10 @@
+module Types exposing (Instruction(..), Program)
+
+type Instruction
+    = Forward Int
+    | Left Int
+    | Right Int
+    | Repeat Int (List Instruction)
+
+type alias Program =
+    List Instruction
